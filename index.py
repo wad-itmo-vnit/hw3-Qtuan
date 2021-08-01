7afec08592b35e1e1f38e82f04314809dd191cb3
quangtuanitmo18 <67545747+quangtuanitmo18@users.noreply.github.com>
2021-08-02 02:43:35 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, render_template, redirect, url_for, request
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route('/')
+def welcome():
+    return redirect('/login')
+ 
+ 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+ 
+ 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+ 
+ 
+if __name__ == '__main__':
+    app.run( host="localhost" ,port=5000, debug=True)
